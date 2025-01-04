a6b525c2ad350de96b4c8ac54ea9384983a12183
Masud-Rana-1002 <masudrana19981002@gmail.com>
2025-01-04 23:40:51 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
 import Navbar from './components/Header/Navbar'
 import Banner from './components/Header/Banner'
 import AboutMe from './components/AboutMe/AboutMe'
--- src/components/DownloadButton/DownloadButton.jsx
+++ src/components/DownloadButton/DownloadButton.jsx
@@ -4,11 +4,11 @@ const DownloadButton = () => {
   return (
     <div>
       <a
-        href="../../assets/webCV.pdf" // Path to your PDF file in the public folder
-        download="YourFile.pdf" // Specify the default name for the downloaded file
+        href="/webCV.pdf" 
+        download="YourFile.pdf" 
       >
         <button className="hover:bg-white text-[#1c1f23] bg-gray-300 hover:text-[#1c1f23] py-2 px-6 rounded-md shadow-md transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-white/50">
-        Download My Resume
+          Download My Resume
         </button>
       </a>
     </div>

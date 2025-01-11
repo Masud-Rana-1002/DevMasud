de75b4429285e1334f264e48aeff777429dc9062
Masud-Rana-1002 <masudrana19981002@gmail.com>
2025-01-11 23:53:41 +0300

    update Masud_Rana_Web_Developer_Resume

--- src/components/DownloadButton/DownloadButton.jsx
+++ src/components/DownloadButton/DownloadButton.jsx
@@ -4,8 +4,8 @@ const DownloadButton = () => {
   return (
     <div>
       <a
-        href="/webCV.pdf" 
-        download="YourFile.pdf" 
+        href="/public/Masud_Rana_Web_Developer_Resume.pdf" 
+        download="Masud_Rana_Web_Developer_Resume" 
       >
         <button className="hover:bg-white text-[#1c1f23] bg-gray-300 hover:text-[#1c1f23] py-2 px-6 rounded-md shadow-md transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-white/50">
           Download My Resume

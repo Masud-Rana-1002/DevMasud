6260d34fbd096f1ba2875eaabad15344a20276f7
Masud-Rana-1002 <masudrana19981002@gmail.com>
2025-02-01 18:38:58 +0300

    add new project

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -72,9 +72,9 @@ const Navbar = () => {
 
   return (
     <nav className="sticky z-50 px-8 py-2 top-0 flex mx-auto items-center justify-between container md:w-full backdrop-blur-md bg-black/30 border-b border-white/10 rounded-full shadow-lg transition-all duration-300">
-      <div className="flex items-center gap-2">
+    <Link to='/'>  <div className="flex items-center gap-2">
         <img className="w-8" src={logo} alt="logo" /> <p>DevMasud</p>
-      </div>
+      </div></Link>
 
       <ul className="items-center gap-[20px] text-[1rem] text-[#424242] lg:flex hidden">
         {navMenu}
